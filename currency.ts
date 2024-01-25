b6983e964499259714624b7a491dc6745ea6d7e2
999 <999@hyeon.pro>
2024-01-26 02:19:36 +0900

    Update currency.ts

--- currency.ts
+++ currency.ts
@@ -1,3 +1,19 @@
+i18n.services.formatter?.add('currency', (value, lng, options) => {
+  // 기본값을 currency로 설정한다.
+  if (!options?.style) {
+    options.style = 'currency'
+  }
+  if (options?.removeCountry && options?.currencyDisplay === 'name') {
+    const getCurrencyName = new Intl.NumberFormat(lng, options).format(value)
+    const result = getCurrencyName.split(' ')
+
+    // 기본 template
+    const template = options?.template || '[[currency]]'
+    return pipe(template, S.replace('[[currency]]', result.slice(-1)[0]))
+  }
+  return new Intl.NumberFormat(lng, options).format(value)
+})
+
 /**
  * next-i18next에서는 해당 formatter를 추가하는 방식이 재대로 동작하지는 않고있다.
  * TODO: 오류 수정해야함. 지금 당장 해결하기에는 중요도가 낮아서 다음에 진행하는 것으로
@@ -10,37 +26,40 @@
 interface CustomOptions extends Intl.NumberFormatOptions {
   removeCountry?: boolean
 }
-i18n?.services.formatter?.add(
-  'currency',
-  (value, lng, options: CustomOptions | undefined) => {
-    const overrideOptions: Intl.NumberFormatOptions = { ...options }
-    // 기본값을 currency로 설정한다.
-    if (options && !options?.style) {
-      overrideOptions.style = 'currency'
-    }
+i18n.services.formatter?.add('price', (value, lng, options) => {
+  const overrideOptions = { ...options }
+  // 기본값을 currency로 설정한다.
+  if (!overrideOptions?.style) {
+    overrideOptions.style = 'currency'
+  }
 
-    // 대한민국 통화 커스텀 포맷터
-    // Javascript의 기본 Intl.NumberFormat에서 name의 값은 `대한민국 원`으로만 지원한다.
-    // 하지만우리는 `1,000 원`을 원한다.
-    // 모든 나라 화폐에 대해서 동일하게 화페 단위만 가져올 것이다.
-    // - 대한민국 원
-    // - 미국 달러
-    // - 유로
-    // - 일본 엔화
-    if (options?.removeCountry && options?.currencyDisplay === 'name') {
-      const getCurrencyName = new Intl.NumberFormat(
-        lng,
-        overrideOptions,
-      ).format(value)
-      const result = getCurrencyName.split(' ')
-      // 가격제거
-      const price = result.shift()
+  // 대한민국 통화 커스텀 포맷터
+  // Javascript의 기본 Intl.NumberFormat에서 name의 값은 `대한민국 원`으로만 지원한다.
+  // 하지만우리는 `1,000 원`을 원한다.
+  // 모든 나라 화폐에 대해서 동일하게 화페 단위만 가져올 것이다.
+  // - 대한민국 원
+  // - 미국 달러
+  // - 유로
+  // - 일본 엔화
+  if (
+    overrideOptions?.removeCountry &&
+    overrideOptions?.currencyDisplay === 'name'
+  ) {
+    const getCurrencyName = new Intl.NumberFormat(
+      lng,
+      overrideOptions
+    ).format(value)
+    const result = getCurrencyName.split(' ')
+    // 가격제거
+    const price = result.shift()
 
-      if (result.length > 1) {
-        return `${price} ${result[1]}`
-      }
-      return `${price} ${result[0]}`
-    }
-    return new Intl.NumberFormat(lng, overrideOptions).format(value)
-  },
-)
+    // 기본 template
+    const template = overrideOptions?.template || '[[amount]] [[currency]]'
+    return pipe(
+      template,
+      S.replace('[[amount]]', String(price)),
+      S.replace('[[currency]]', result.slice(-1)[0])
+    )
+  }
+  return new Intl.NumberFormat(lng, overrideOptions).format(value)
+})
